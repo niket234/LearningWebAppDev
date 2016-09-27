46535963a14d77f05caeb69def1767fa8ecdf879
Niket K <niket92@csu.fullerton.edu>
2016-09-27 13:15:34 -0700

    Modified files for Chapter4

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,45 +1,38 @@
 /* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
-var main = function () {
+var main = function() {
     'use strict';
-
-    var addCommentFromInputBox = function (val,clas) {
-        var $newComment=null;
-
-        if (val!== '') {
-            $newComment = $('<p class='+clas+'>').text(val);
+    var addCommentFromInputBox = function(val, clas) {
+        var $newComment = null;
+        if (val !== '') {
+            $newComment = $('<p class=' + clas + '>').text(val);
             $newComment.hide();
             $('.comments').append($newComment);
             $newComment.fadeIn();
-            
         }
     };
-
-    $('.comment-input button').on('click', function () {
-        addCommentFromInputBox($('.comment-input input').val(),'user');
+    $('.comment-input button').on('click', function() {
+        addCommentFromInputBox($('.comment-input input').val(),
+            'user');
         $('.comment-input input').val('');
     });
-
-    $('.comment-input input').on('keypress', function (event) {
+    $('.comment-input input').on('keypress', function(event) {
         if (event.keyCode === 13) {
-         addCommentFromInputBox($('.comment-input input').val(),'user');
-        $('.comment-input input').val('');
+            addCommentFromInputBox($('.comment-input input').val(),
+                'user');
+            $('.comment-input input').val('');
         }
     });
-
-
-    $('.comment-input-user2 button').on('click', function () {
-        addCommentFromInputBox($('.comment-input-user2 input').val(),'user2');
+    $('.comment-input-user2 button').on('click', function() {
+        addCommentFromInputBox($('.comment-input-user2 input').val(),
+            'user2');
         $('.comment-input-user2 input').val('');
     });
-
-    $('.comment-input-user2 input').on('keypress', function (event) {
+    $('.comment-input-user2 input').on('keypress', function(event) {
         if (event.keyCode === 13) {
-         addCommentFromInputBox($('.comment-input-user2 input').val(),'user2');
-        $('.comment-input-user2 input').val('');
+            addCommentFromInputBox($('.comment-input-user2 input').val(),
+                'user2');
+            $('.comment-input-user2 input').val('');
         }
     });
-
-
 };
-
 $(document).ready(main);
